22333280ebd1176f5e068fd202b04f4f11c131bd
Fedi GALFAT <55464780+fozz101@users.noreply.github.com>
2023-09-19 22:44:26 +0100

    Added Email Addresses of current volunteers

--- IEEE Missing Volunteers/script.py
+++ IEEE Missing Volunteers/script.py
@@ -4,14 +4,13 @@ Created on Fri Jan  7 14:22:09 2023
 
 @author: fedig
 """
-
-import time
+'''
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
 
 
 #members = pd.read_excel("excelFileName.xlsx")
-members = pd.read_excel("VolunteerListUpdatedStatus1.xlsx")
+members = pd.read_excel("Volunteer List by OU .xlsx")
 
 all_positions = ["Chair", "Counselor", "Secretary", "Treasurer", "Vice Chair", "Webmaster"]
 
@@ -23,4 +22,30 @@ def find_missing_positions(positions):
 grouped["Missing Positions"] = grouped["Position"].apply(find_missing_positions)
 
 
-grouped.to_excel("Missing Volunteers.xlsx")
+grouped.to_excel("SBs Missing Volunteers.xlsx")
+'''
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
+# Read the Excel file
+members = pd.read_excel("Volunteer List by OU.xlsx")
+
+all_positions = ["Chair", "Advisor", "Secretary", "Treasurer", "Vice Chair", "Webmaster"]
+
+# Group by OU Name and aggregate email addresses into a comma-separated string
+grouped = members.groupby("OU Name").agg({
+    "Position": list,
+    "Email Address  ": lambda x: ','.join(x) if not x.empty else ''
+}).reset_index()
+
+# Define a function to find missing positions
+def find_missing_positions(positions):
+    return [pos for pos in all_positions if pos not in positions]
+
+# Apply the function to create a new column for missing positions
+grouped["Missing Positions"] = grouped["Position"].apply(find_missing_positions)
+
+# Save the result to an Excel file
+grouped.to_excel("SBCs Missing Volunteers.xlsx", index=False)
+
+
